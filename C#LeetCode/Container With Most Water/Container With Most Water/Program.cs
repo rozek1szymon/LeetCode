2cb85dff7f3ed51fd932d4fa62c9bf8ec3ca1386
Szymon Rozek <rozek1szymon@gmail.com>
2019-05-10 20:30:41 +0200

    new staff

---
+++ C#LeetCode/Container With Most Water/Container With Most Water/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container_With_Most_Water
+{
+
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            var whichElement=0;
+            var finalCapacity = 0;
+            var capacity = 0;
+            var whichLoop = 1;
+            for (int i = whichLoop; i < height.Length; i++)
+            {                             
+                if(height[whichElement]<height[i])
+                {
+                    capacity = height[whichElement] * (i-whichElement);
+                }
+                else
+                {
+                    capacity = height[i] * (i - whichElement);
+                }
+                //whichElement++;
+                if (capacity > finalCapacity)
+                    finalCapacity = capacity;
+                if (height.Length-1 == i)
+                {
+                    i = whichLoop++;
+                    whichElement++;
+                }
+            }
+                            
+            return finalCapacity;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var solution = new Solution();
+            solution.MaxArea(height);
+            Console.ReadKey();
+        }
+    }
+}
