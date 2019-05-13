f732df27491b59f88c9054fab53ae19cfee5c5f9
Szymon Rozek <rozek1szymon@gmail.com>
2019-05-13 19:30:27 +0200

    Added some new staff

--- C#LeetCode/Top K Frequent Elements/Top K Frequent Elements/Program.cs
+++ C#LeetCode/Top K Frequent Elements/Top K Frequent Elements/Program.cs
@@ -6,10 +6,39 @@ using System.Threading.Tasks;
 
 namespace Top_K_Frequent_Elements
 {
+
+    public class Solution
+    {
+        public IList<int> TopKFrequent(int[] nums, int k)
+        {
+            //var nums= nums.OrderBy(x=>x).ToArray();
+            Dictionary<int, int> dictOfValues = new Dictionary<int, int>();
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(dictOfValues.Keys.Contains(nums[i]))
+                {
+                    dictOfValues[nums[i]]++;
+                }
+                else
+                {
+                    dictOfValues.Add(nums[i], 1);
+                }
+            }
+          
+            return dictOfValues.OrderByDescending(x => x.Value).Select(x => x.Key).Take(k).ToList();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            var solution = new Solution();
+            int k = 2;
+            int[] nums =  { 1, 5,6,5 ,5,1, 2, 2, 2 };
+            Console.WriteLine(solution.TopKFrequent(nums, k)); 
+            Console.ReadKey();
+
         }
     }
 }
